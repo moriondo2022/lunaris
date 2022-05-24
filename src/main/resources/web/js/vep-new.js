d632458a7c07760042e326c227951b5851673ede
Annie Moriondo <amoriond@broadinstitute.org>
2022-05-24 13:01:02 -0400

    further troubleshooting - why won't show-session display filename?

--- src/main/resources/web/js/vep-new.js
+++ src/main/resources/web/js/vep-new.js
@@ -97,10 +97,9 @@ function loadSession(sessionId) {
                 }
                 setEmptySubmissionArea();
                 session.jobs.forEach(job => {
-                    console.log("Adding job " + job.id);
                     const id = job.id;
-                    const path = job.inputFile;
-                    const inputFileName = path.substring(path.lastIndexOf("/") + 1);
+                    const inputFileName = trimFilename(job.inputFile);
+                    console.log("Adding job " + job.id + " " + inputFileName);
                     addStatusEntry(inputFileName, id);
                 });
                 setSessionMsg("Loading session " + sessionId + ".");
@@ -461,8 +460,8 @@ function soManyErrors(nSnags) {
 function showStatus(id) {
     const statusRow = document.getElementById(id);
     const outputFileCell = statusRow.getElementsByClassName("output-file-cell")[0];
-    const inputFileName = lunarisVariantPredictor.inputFileNames[id];
-    const status = lunarisVariantPredictor.statuses[id];
+    let inputFileName = lunarisVariantPredictor.inputFileNames[id];
+    let status = lunarisVariantPredictor.statuses[id];
     if (status) {
         if (status.succeeded) {
             console.log("Success for " + id);
